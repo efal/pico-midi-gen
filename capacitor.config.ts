24647acefba6ec624d09f8906436a6252c2336f7
efal <32618426+efal@users.noreply.github.com>
2025-12-12 19:57:46 +0100

    Delete capacitor.config.ts

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.jambuddy.app',
-  appName: 'Jam Buddy',
-  webDir: 'dist'
-};
-
-export default config;
